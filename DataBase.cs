ce2620a855761493b756ed30b0f72eeab8a33fcb
Natejin <38517532+Natejin@users.noreply.github.com>
2020-03-18 13:05:56 +0900

    QustionAboutItemData
    ItemData for database archtecture

---
+++ DataBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataBase : MonoBehaviour
+{
+
+    public List<Item_Data> callme;
+}
---
+++ Data_Item.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[System.Serializable]
+public class Data_Item
+{
+    public int ID;
+    public ItemMainType itemType;
+
+    public Data_Item(Data_Item data_Item)
+    {
+        this.ID = data_Item.ID;
+        this.itemType = data_Item.itemType;
+    }
+
+
+    public Data_Item(int ID, ItemMainType itemType)
+    {
+        this.ID = ID;
+        this.itemType = itemType;
+    }
+}
+
+[System.Serializable]
+public class Data_Item_Equip : Data_Item
+{
+    public int durability;
+
+
+    public Data_Item_Equip(Data_Item_Equip data_Item) : base(data_Item)
+    {
+        this.ID = data_Item.ID;
+        this.itemType = data_Item.itemType;
+        this.durability = data_Item.durability;
+    }
+
+    public Data_Item_Equip(Data_Item data_Item) : base(data_Item)
+    {
+        this.ID = data_Item.ID;
+        this.itemType = data_Item.itemType;
+        //this.durability = 10;
+    }
+
+    public Data_Item_Equip(int ID, ItemMainType itemType, int durability) : base(ID, itemType)
+    {
+
+        this.ID = ID;
+        this.itemType = itemType;
+        this.durability = durability;
+
+    }
+}
+
+[System.Serializable]
+public class Data_Item_Use : Data_Item
+{
+    public ItemEffect itemEffect;
+    public int amount;
+
+    public Data_Item_Use(Data_Item_Use data_Item) : base(data_Item)
+    {
+        this.ID = data_Item.ID;
+        this.itemType = data_Item.itemType;
+        this.amount = data_Item.amount;
+    }
+
+    public Data_Item_Use(int ID, ItemMainType itemType, int amount) : base(ID, itemType)
+    {
+        this.ID = ID;
+        this.itemType = itemType;
+        this.amount = amount;
+    }
+}
+
+
+
+[System.Serializable]
+public class Data_Item_Equip_Weapon : Data_Item_Equip
+{
+    public int damage;
+
+    public Data_Item_Equip_Weapon(Data_Item_Equip_Weapon data_Item) : base(data_Item)
+    {
+        this.ID = data_Item.ID;
+        this.itemType = data_Item.itemType;
+        this.durability = data_Item.durability;
+        this.damage = data_Item.damage;
+    }
+
+    public Data_Item_Equip_Weapon(int ID, ItemMainType itemType, int durability, int damage) : base(ID, itemType, durability)
+    {
+
+        this.ID = ID;
+        this.itemType = itemType;
+        this.durability = durability;
+        this.damage = damage;
+
+    }
+}
+
+[System.Serializable]
+public class Data_Item_Equip_Armor : Data_Item_Equip
+{
+    public int armor;
+
+    public Data_Item_Equip_Armor(Data_Item_Equip_Armor data_Item) : base(data_Item)
+    {
+        this.armor = data_Item.armor;
+    }
+
+    public Data_Item_Equip_Armor(int ID, ItemMainType itemType, int durability, int armor) : base(ID, itemType, durability)
+    {
+        this.armor = armor;
+    }
+}
